e9dbddb2bd985ec7b14a22e2ee7aef52a0665e4b
alejandroPolak <alejandro_polak@hotmail.com>
2020-05-01 13:30:31 -0300

    [Stylecomponents] ConteinerHeader y TextHeader

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React from 'react'
 import styled from '@emotion/styled'
 
+const ConteinerHeader = styled.header`
+  background-color: #26c6da;
+  padding: 10px;
+  font-weight: bold;
+  color: #ffffff;
+`
 
+const TextHeader = styled.h1`
+  font-size: 2rem;
+  margin: 0;
+  font-family: 'Slabo 27px', serif;
+  text-align: center;
+`
 
-const Header = ({title}) => {
-    return ( 
-        <header>
-            <h1>{title}</h1>
-        </header>
-     );
+const Header = ({ title }) => {
+  return (
+    <ConteinerHeader>
+      <TextHeader>{title}</TextHeader>
+    </ConteinerHeader>
+  )
 }
- 
-export default Header;
+
+export default Header
