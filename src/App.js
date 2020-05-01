04eec9bed3c19099507cb42e4a831f61521f1590
alejandroPolak <alejandro_polak@hotmail.com>
2020-05-01 13:38:33 -0300

    [StyleComponentes]
    Conteiner y ConteinerForm

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import Header from './components/Header'
 
+import styled from '@emotion/styled'
+
+const Conteiner = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`
+const ConteinerForm = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`
+
 function App() {
-  return <Header title="Cotizador de Seguros" />
+  return (
+    <Conteiner>
+      <Header title="Cotizador de Seguros" />
+      <ConteinerForm>
+        
+      </ConteinerForm>
+    </Conteiner>
+  )
 }
 
 export default App
